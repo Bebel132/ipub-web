767a7958fef5845663c538bd203a9525baa4b4b8
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2026-06-27 09:00:40 -0300

    corrige macacada

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const createApiInstance = (): AxiosInstance => {
     return axios.create({
-        baseURL: import.meta.env.API_URL,
+        baseURL: import.meta.env.VITE_API_URL,
     })
 }
 
